7fe63549d0ef3a90e367b4b788c4921f4dd4d608
davidpeeler9 <davidpeeler9@gmail.com>
2017-03-20 19:32:52 -0400

    Create Master.py

---
+++ Master.py
@@ -0,0 +1,20 @@
+import list
+
+DivisorsList = []
+
+while True
+  InputNumber = input('What number would you like to test?')
+  if isinstance(InputNumber, int) = True
+    break
+
+
+def PrimalityTest(InputNumber):
+  for num in range(1, InputNumber/2):
+    if InputNumber%num == 0:
+      DevisorsList.append(num)
+      
+
+
+      
+      
+  
