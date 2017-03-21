4a0fd449fbfd30dbaf6bab553f21e958fa258846
davidpeeler9 <davidpeeler9@gmail.com>
2017-03-20 21:24:19 -0400

    Update Master.py

--- Master.py
+++ Master.py
@@ -1,26 +1,28 @@
 import math
 
-IsPrime = 0
-IsPerfectSquare = 0
+Prime = 0
+PerfectSquare = 0
 DivisorsList = []
 PFList = []
 
-while True:
-  InputNumber = input("What number would you like to test?")
-  if isinstance(InputNumber, int) == True:
-	  break
+InputNumber = input("What number would you like to test?")
+
+#while True:
+#  InputNumber = input("What number would you like to test?")
+#  if isinstance(InputNumber, int) == True:
+#	  break
 
 def PrimalityTest(y):
   for num in range(1, y):
     if InputNumber%num == 0:
-      DevisorsList.append(num)
+      DivisorsList.append(num)
   return
 
 def IsPerfectSquare(x):
   if math.sqrt(x)%2 == 0:
-    IsPerfectSquare = 1
+    PerfectSquare = 1
   else:
-      IsPerfectSquare = 0
+      PerfectSquare = 0
   return
 
 def PrimeFactorization(): #TODO, change the divisible factors of PFList to primes
@@ -32,21 +34,21 @@ def PrimeFactorization(): #TODO, change the divisible factors of PFList to prime
 	return
 
 PrimalityTest(InputNumber)
-
 IsPerfectSquare(InputNumber)
 PrimeFactorization()
 
 
-if IsPrime == 0:
+if Prime == 0:
 	print("% is not a prime number") % InputNumber
 else:
 	print("% is a prime number") % InputNumber
 
 
-if IsPerfectSquare == 0:
+if PerfectSquare == 0:
 	print("% is not a perfect square") % InputNumber
 else:
 	print("% is a perfect square") % InputNumber
 
 
 print("%'s prime factorization is %") % (InputNumber, PFList)
+
