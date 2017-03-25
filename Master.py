d398196d6cad9d56f803b4268e054956c9024316
davidpeeler9 <davidpeeler9@gmail.com>
2017-03-25 19:05:45 -0400

    Update Master.py

--- Master.py
+++ Master.py
@@ -1,37 +1,41 @@
 # TODO:
 # BaseChanger
 # PrimeFactorization
-# SplitNumber
 # Doesnt check if the menu input is 1-5
 # Clear the console after every test
-#
+# Doesnt repeat to menu indefinately
+
 # Finished:
 # Menu
 # PrimalityTest
+# SplitNumber
 # PerfectSquare
-# Doesnt repeat to menu indefinately
+# CircularPrime
 
 import math
 import os
-
-
+import itertools
 
 def cls():
   os.system('cls' if os.name=='nt' else 'clear')
   return
 
 def isPrime(x):
-  for num in range(2, x//2):
+  FactorList = []
+  x = int(x)
+  for num in range(2, math.ceil(x//2)):
     if x%num == 0:
-      return False
-    else:
-      return True 
+      FactorList.insert(1, num)
+  if len(FactorList) == 0:
+    return True
+  else:
+    return False
 
 def SplitNumber(x):
   x = str(x)
   DigitList = []
   for y in range(len(x)):
-    DigitList.insert(1, x[y])
+    DigitList.insert(1, (x[y]))
   return DigitList
 
 def FindTrueFactors():
@@ -45,7 +49,7 @@ def BaseChanger():
   y = int(input("What number would you like to test?"))
   x = int(input("What base is your starting number?"))
   z = int(input("What base would you like to change into?"))
-  return #TODO
+  return #TODO #TODO
 
 def IsPerfectSquare():
   y = int(input("What number would you like to test?"))
@@ -56,31 +60,23 @@ def IsPerfectSquare():
   return
 
 def PrimeFactorization(): 
-  y = int(input("What number would you like to test?")) # TODO
-
-def CircularPrime(): #TODO
-  y = input("What number would you like to test?")
+  y = int(input("What number would you like to test?"))
   
-  # Test if the number is prime
+  # Testing if it is prime --> true factors would be its PrimeFactorization
   if isPrime(y) is True:
-    return print(str(y) + "is not a circular prime.")
+    print("The prime factorization for this number is 1 and " + str(y))
   
-  DigitList = SplitNumber(y)
-  tests = len(SplitNumber(y)) #test every number of digits till tests?
-  
-  for x in range(tests):
-    for num in DigitList:
-      
-  
-  
-  
-  #Test if the individual digits are prime  
-  #for x in SplitNumber(y):
-  #  testNumber = int()
-  
-  #Test if the two number digits are prime
-  #  testNumber = int(str(x) + str())
-        return #TODO #TODO
+def CircularPrime():
+  y = int(input("What number would you like to test?"))
+  comb = list(map("".join, itertools.permutations(str(y))))
+  for item in comb:
+    if isPrime(item) is False:
+      print(str(y) + " is not a circular prime.")
+      break
+  else:
+    print(str(y) + " is a circular prime.")
+  return
+
 
 
 # Menu
@@ -105,15 +101,13 @@ def Menu():
   if choice == "5":
     CircularPrime()
   
-  
-  cls()
-	
   choice2 = input("Would you like to run another test?")
-  if choice2 == "yes" or "y" or "ye":
+  if choice2 == "yes":
     Menu()
   else:
     print("Bye Bye!")
 
   return
+
 
 Menu()
